322c42ec8466e61de8f384354fde4f6119501d6e
kreena <kreena@NOIKREENA31356>
2016-08-24 14:17:51 +0530

    Test Code

---
+++ TestCApp/TestCApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int AB = Add(10, 20);
+        }
+        
+        static int Add(int A,int B)
+        {
+            return A + B;
+        }
+    }    
+}
